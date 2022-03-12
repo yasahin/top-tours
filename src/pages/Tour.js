3d2c2f3b0be096c41b3d062635a7e9833e4c8622
yasahin <93654754+yasahin@users.noreply.github.com>
2022-03-12 21:47:22 +0300

    top-tours-(MUI)
    I made this project to improve my use of Material UI. You can book the most popular tours here.

---
+++ src/pages/Tour.js
@@ -0,0 +1,66 @@
+import Container from "@mui/material/Container";
+import { Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+import ImageCollage from "../components/ImageCollage";
+import CustomizedAccordions from "../components/Accordian";
+import Paper from "@mui/material/Paper";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BasicModal from "../components/Modal";
+
+const Tour = () => (
+  <Container sx={{ width: 900 }}>
+    <Typography variant="h3" component="h1" marginTop={3}>
+      Explore the World in Vegas
+    </Typography>
+    <Box marginTop={3} sx={{ display: "flex" }}>
+      <img
+        src="https://media.timeout.com/images/105124791/750/422/image.jpg"
+        height={325}
+        alt=""
+      />
+      <ImageCollage />
+    </Box>
+    <Box>
+      <Typography variant="h6" component="h4" marginTop={3}>
+        About this ticket
+      </Typography>
+      <Typography variant="paragraph" component="p" marginTop={3}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged.
+      </Typography>
+    </Box>
+    <Box marginBottom={10}>
+      <Typography variant="h6" component="h4" marginTop={3}>
+        Frequently Asked Questions
+      </Typography>
+      <Typography
+        variant="paragraph"
+        component="p"
+        marginTop={3}
+        marginBottom={2}
+      >
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged.
+      </Typography>
+      <CustomizedAccordions />
+    </Box>
+    <Paper
+      sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+      elevation={3}
+    >
+      <BottomNavigation>
+        <BasicModal />{" "}
+      </BottomNavigation>
+    </Paper>
+  </Container>
+);
+
+export default Tour;
